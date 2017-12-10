98fe66f88a9737881038b4dbe44e52f2d7094434
Shane Parker <smparker@uci.edu>
2017-12-10 13:30:42 -0800

    ignore problematic url

--- test.sh
+++ test.sh
@@ -8,5 +8,6 @@ htmlproofer --check-html \
             --check-favicon \
             --check-opengraph \
             --empty-alt-ignore \
+            --url-ignore 'https://scholar.google.com/citations?user=OqC2Vc8AAAAJ&hl=en' \
             --url-swap "https?\:\/\/(localhost\:4000|smparker\.github\.io):" \
             _site
