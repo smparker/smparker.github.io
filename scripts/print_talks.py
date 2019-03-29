4cc0cb75598f324144d5f556b8b2b82c9f30c96d
Shane Parker <smparker@uci.edu>
2019-03-29 15:40:39 -0700

    include script to print from yaml

---
+++ scripts/print_talks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import yaml
+
+data = yaml.load(open("lectures.yml"))
+
+print(data)
+for x in data:
+    talktype = "invited talk" if x["invited"] else "talk"
+    print("@Misc{")
+    print("    OPTkey =   {},")
+    print("    author =   { \\underline{Shane M. Parker} and Filipp Furche },")
+    print("    title =   {{{{ {0} }}}},".format(x["title"]))
+    print("    howpublished =   {{{0}, {1}, {2}}},".format(talktype, x["institution"], x["location"]))
+    print("    OPTmonth = {},")
+    print("    year = {{{0}}},".format(x["time"]))
+    print("    OPTnote = {},")
+    print("    OPTannote = {}")
+    print("}")
