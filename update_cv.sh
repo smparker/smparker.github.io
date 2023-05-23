142862ac7f51020e7fedefa0924b00590eb54850
Shane Parker <shane.parker@case.edu>
2023-05-23 15:25:23 -0400

    update cv script

---
+++ update_cv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd latex_cv
+  ./texify smp_cv
+  cp smp_cv.pdf ../assets/smp_cv.pdf
+cd ..
+
+echo "date: $(date +"%m/%d/%Y")" > _data/cv.yml
+echo "file: assets/smp_cv.pdf" >> _data/cv.yml
